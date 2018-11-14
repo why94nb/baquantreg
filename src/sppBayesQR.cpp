42f4a6df2a4446b8ba7ca74c4da2e8b7a21b0da5
why94nb <why94nb@gmail.com>
2018-11-14 02:06:44 -0500

    paicha

--- src/sppBayesQR.cpp
+++ src/sppBayesQR.cpp
@@ -111,8 +111,7 @@ List sppBayesQR(double tau, arma::colvec y, arma::mat X, int itNum,
 		   sigmaValue = rinvgammaRcpp(nTilde / 2, sTilde / 2);
 
 		   for (int o = 0; o < n; o++) {
-			   zSample[o] = mtM(y - X * betaValue, theta, psi2, sigmaValue, zSample,
-				   zSample[o], o, CovCov, tuneV, kMT);
+			   zSample[o] =1;
 		   }
 
 		   if (discLambda) {
